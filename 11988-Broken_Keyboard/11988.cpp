fa61ed8fc85720410558b95c9d4360abd283110c
Franco Ramírez Reyes <20143156@aloe.ulima.edu.pe>
2017-11-24 03:23:41 +0100

    Now captures full line instead of character by character, added cstring.

--- 11988-Broken_Keyboard/11988.cpp
+++ 11988-Broken_Keyboard/11988.cpp
@@ -1,20 +1,22 @@
 #include <cstdio>
 #include <list>
+#include <cstring>
 using namespace std;
 int main(){
+  char buffer[100001];
   list<char> line;
   list<char>::iterator it;
-  char c=0;
-  while(c != EOF && scanf("%c",&c) != EOF){
+  int length;
+  while(scanf("%s",buffer) != EOF){
     it = line.begin();
-    while(c != '\n'){
-      if(c == '[')
+    length=strlen(buffer);
+    for(int i = 0; i < length; i++){
+      if(buffer[i] == '[')
         it = line.begin();
-      else if(c == ']')
+      else if(buffer[i] == ']')
         it = line.end();
       else
-        line.insert(it, c);
-      scanf("%c",&c);
+        line.insert(it, buffer[i]);
     }
     for(it = line.begin(); it != line.end(); it++)
       printf("%c",*it);
